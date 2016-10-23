6339cee015ebb5418306c883b63e629c4628ff96
Sergey <serg3d@hotmail.com>
2016-10-23 12:46:59 -0700

    added test project

---
+++ test/artTest01/src/main/java/test001.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+
+/**
+ * Created by IEUser on 10/23/2016.
+ */
+public class test001 {
+
+    @Test
+    public static void print() {
+        System.out.println("Hello World");
+    }
+}
